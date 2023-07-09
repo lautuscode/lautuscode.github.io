79958b181684d2d21cec8e676e35d9bb305af413
Brian Burkard <brianburkard@gmail.com>
2023-07-09 14:15:23 -0400

    deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lautuscode.github.io',
   integrations: [compress(), mdx(), tailwind()],
 })
